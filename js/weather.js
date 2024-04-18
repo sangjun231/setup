0565b5b51c778193b7c116a889c2076a256ec15b
Eun <chorockmasil@gmail.com>
2024-04-18 09:34:23 +0900

    all weathers

--- js/weather.js
+++ js/weather.js
@@ -1,81 +1,29 @@
 const API_KEY = "0b2c4bafac50aa0e8715062acb786c5f";
 
-const translation_kr = {
-  thunderstorm_with_light_rain: "가벼운 비를 동반한 천둥구름",
-  thunderstorm_with_rain: "비를 동반한 천둥구름",
-  thunderstorm_with_heavy_rain: "폭우를 동반한 천둥구름",
-  light_thunderstorm: "약한 천둥구름",
-  thunderstorm: "천둥구름",
-  heavy_thunderstorm: "강한 천둥구름",
-  ragged_thunderstorm: "불규칙적 천둥구름",
-  thunderstorm_with_light_drizzle: "약한 연무를 동반한 천둥구름",
-  thunderstorm_with_drizzle: "연무를 동반한 천둥구름",
-  thunderstorm_with_heavy_drizzle: "강한 안개비를 동반한 천둥구름",
-  light_intensity_drizzle: "가벼운 안개비",
-  drizzle: "안개비",
-  heavy_intensity_drizzle: "강한 안개비",
-  light_intensity_drizzle_rain: "가벼운 적은비",
-  drizzle_rain: "적은비",
-  heavy_intensity_drizzle_rain: "강한 적은비",
-  shower_rain_and_drizzle: "소나기와 안개비",
-  heavy_shower_rain_and_drizzle: "강한 소나기와 안개비",
-  shower_drizzle: "소나기",
-  light_rain: "악한 비",
-  moderate_rain: "중간 비",
-  heavy_intensity_rain: "강한 비",
-  very_heavy_rain: "매우 강한 비",
-  extreme_rain: "극심한 비",
-  freezing_rain: "우박",
-  light_intensity_shower_rain: "약한 소나기 비",
-  shower_rain: "소나기 비",
-  heavy_intensity_shower_rain: "강한 소나기 비",
-  ragged_shower_rain: "불규칙적 소나기 비",
-  light_snow: "가벼운 눈",
-  snow: "눈",
-  heavy_snow: "강한 눈",
-  sleet: "진눈깨비",
-  shower_sleet: "소나기 진눈깨비",
-  light_rain_and_snow: "약한 비와 눈",
-  rain: "비",
-  rain_and_snow: "비와 눈",
-  light_shower_snow: "약한 소나기 눈",
-  shower_snow: "소나기 눈",
-  heavy_shower_snow: "강한 소나기 눈",
-  mist: "박무",
-  smoke: "연기",
-  haze: "연무",
-  "sand,_dust_whirls": "모래 먼지",
-  fog: "안개",
-  sand: "모래",
-  dust: "먼지",
-  volcanic_ash: "화산재",
-  squalls: "돌풍",
-  clear: "맑음",
-  clear_sky: "구름 한 점 없는 맑은 하늘",
-  clouds: "구름",
-  few_clouds: "약간의 구름이 낀 하늘",
-  scattered_clouds: "드문드문 구름이 낀 하늘",
-  broken_clouds: "구름이 거의 없는 하늘",
-  overcast_clouds: "구름으로 뒤덮인 흐린 하늘",
-  tornado: "토네이도",
-  tropical_storm: "태풍",
-  cold: "한랭",
-  hot: "고온",
-  windy: "바람부는",
-  hail: "우박",
-  calm: "바람이 거의 없는",
-  light_breeze: "약한 바람",
-  gentle_breeze: "부드러운 바람",
-  moderate_breeze: "중간 세기 바람",
-  fresh_breeze: "신선한 바람",
-  strong_breeze: "센 바람",
-  high_win: "돌풍에 가까운 센 바람",
-  gale: "돌풍",
-  "severe gale": "심각한 돌풍",
-  storm: "폭풍",
-  violent_storm: "강한 폭풍",
-  hurricane: "허리케인",
+// open weather api 에서 return 하는 json 중 
+// data.weather[0].main 의 모든 프로퍼티
+// 오른쪽의 값은 openweathermap 에서 제공하는 날씨별 icon 코드
+const allMainWeathers = {
+  Thunderstorm : "11d",
+  Drizzle : "09d",
+  Rain : "09d",
+  Snow : "13d", 
+  Mist : "50d",
+  Smoke : "50d",
+  Haze : "50d",
+  Dust : "50d",
+  Fog : "50d",
+  Sand : "50d",
+  Dust : "50d",
+  Ash : "50d",
+  Squall : "50d",
+  Tornado : "50d",
+  Clear : "10d",
+  Clouds : "04d",
 };
+
+// 아이콘 url 예시(현재 맑음일 경우)
+let iconUrl = `https://openweathermap.org/img/wn/${allMainWeathers.Clear}@2x.png`
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
@@ -93,6 +41,7 @@ function onGeoOk(position) {
       const weatherSection = document.getElementById("weather-section");
       city.innerText = data.name;
 
+      console.log(data.weather[0])
       const weatherImage = document.createElement("img");
       switch (data.weather[0].main) {
         case "Clouds":
